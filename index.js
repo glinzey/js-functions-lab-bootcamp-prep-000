ea6cdc3c651021d10945cb722beee9df76bd0551
Gregg Linzey <glinzey1@gmail.com>
2018-04-02 01:38:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,7 @@ function happyHolidayTo(holiday, name){
 }
 function holidayCountdown(holiday, days){
   return (`It's ${days}days until ${holiday}!`)
+  var days=("20")
+  var holiday=("Mother's Day")
 }
+
